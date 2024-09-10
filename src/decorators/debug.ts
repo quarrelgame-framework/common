f1621d1df7f068c82bc5f78dbc353424f49e7fd9
nowaaru@proton.me <vert@leanbox.>
2024-09-10 09:54:14 +0000

    implement debug string formatting
    Signed-off-by: nowaaru@proton.me <vert@leanbox.>

--- src/decorators/debug.ts
+++ src/decorators/debug.ts
@@ -1,10 +1,11 @@
+import { BaseComponent } from "@flamework/components";
 import { Modding, Reflect } from "@flamework/core";
 
 /**
  * Request the required metadata for lifecycle events and dependency resolution.
  * @metadata flamework:implements flamework:parameters
  */
-export const Debug = Modding.createDecorator<[AttributeNames: string[], When?: (() => boolean)]>("Class", (descriptor, [debugParams]) => {
+export const Debug = Modding.createDecorator<[AttributeNames: string[], When?: (() => boolean), Display?: ((key: string, component: BaseComponent) => string)]>("Class", (descriptor, [debugParams]) => {
     if ("onAttributeChanged" in descriptor.object)
     {
         Reflect.defineMetadata(descriptor.object, `qgf.debug`, debugParams)
